2b7a29b3a682a1f51f5ec2834ab1da390be279e1
imnicolas <nicolas.esteban.suarez@hotmail.es>
2023-06-15 18:06:36 -0300

    :lipstick::lipstick::lipstick:

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,20 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
 
+// Modulos
+import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule  } from '@angular/platform-browser/animations'
+
+// Componentes
 import { HomeComponent } from './landing/componentes/home/home.component';
 import { BotonDePagoComponent } from './landing/componentes/boton-de-pago/boton-de-pago.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HomeComponent,
-    BotonDePagoComponent,
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    FormsModule
-  ],
+  declarations: [AppComponent, HomeComponent, BotonDePagoComponent],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, BrowserAnimationsModule],
   providers: [HTMLElement],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
