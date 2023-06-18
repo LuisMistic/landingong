52fb01735b64061cdf872e3750e15b107a195417
luismistic <luis.escalada@hotmail.com>
2023-06-17 23:29:27 -0300

    -

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,18 +3,11 @@ import { RouterModule, Routes } from '@angular/router';
 import { BotonDePagoComponent } from './landing/componentes/boton-de-pago/boton-de-pago.component';
 import { HomeComponent } from './landing/componentes/home/home.component';
 
-<<<<<<< HEAD
-const routes: Routes = [
-  { path: 'mercado-pago', component: BotonDePagoComponent },
-  { path: 'home', component: HomeComponent },
-  { path: '**', pathMatch: 'full', redirectTo: 'home' },
-=======
 import { AppComponent } from './app.component';
 
 const routes: Routes = [
 
   { path: 'index', component: AppComponent }
->>>>>>> 4b4ec186c8d6a1f412d81474b123c4adc332d348
 ];
 
 @NgModule({
