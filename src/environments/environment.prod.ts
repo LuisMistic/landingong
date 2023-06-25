aeb9060e4fcf8b947ddded220ff310572b9efc4a
SuarezNicolas <nicolas.suarez@techforb.com>
2023-06-24 23:09:26 -0300

    Removed environments folder from repository

--- src/environments/environment.prod.ts
+++
@@ -1,8 +0,0 @@
-export const environment = {
-    production: true,
-
-    token: "",
-    publicKey: "APP_USR-0b59bab9-7402-420c-bf92-13fb10f20c25",
-
-    URL: "https://able-charming-garnet.glitch.me"
-}
--- src/environments/environment.ts
+++
@@ -1,8 +0,0 @@
-export const environment = {
-    production: false,
-
-    token: "",
-    publicKey: "TEST-2570476f-179d-421d-b6aa-d57511a3faff",
-
-    URL: "http://localhost:8080"
-}
