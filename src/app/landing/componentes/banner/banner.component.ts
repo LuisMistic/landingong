71c49e3cd7a81c64c5a3352f668f336fdeadfc51
luismistic <luis.escalada@hotmail.com>
2023-06-27 18:49:30 -0300

    banner

--- src/app/landing/componentes/banner/banner.component.ts
+++ src/app/landing/componentes/banner/banner.component.ts
@@ -1,27 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-banner',
   templateUrl: './banner.component.html',
   styleUrls: ['./banner.component.css']
 })
-export class BannerComponent implements OnInit {
+export class BannerComponent {
 
-  constructor() { }
-
-  ngOnInit(): void {
-    this.loadCarouselScript();
-  }
-
-  loadCarouselScript() {
-    const script = document.createElement('script');
-    script.src = './java.js';
-    script.onload = () => {
-      console.log('Script loaded successfully.');
-    };
-    script.onerror = () => {
-      console.error(`Error loading script ${script.src}`);
-    };
-    document.body.appendChild(script);
-  }
 }
